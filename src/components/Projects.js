e6c6b9f18212eb771c12d6ff0d7361735bd8e2a9
nayraromero <nayraromero@gmail.com>
2023-02-01 17:56:25 +0100

    start adding styles to projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,42 @@
 import '../styles/components/projects.scss';
+import Rick from '../images/rick.png';
 
 const Projects = () => {
   return (
     <>
       <section className="projects">
         <h2 className="projects__title">Proyectos</h2>
+        <div className="container">
+          <div className="container__image">
+            <img
+              src={Rick}
+              alt="rick and morty"
+              className="container__image--photo"
+            />
+          </div>
+          <div className="container__info">
+            <h2 className="container__info--title">Title</h2>
+            <p className="container__info--text">Text</p>
+          </div>
+          <div className="container__buttons">
+            <a
+              href="https://www.linkedin.com/in/nayra-romero-salas/"
+              className="container__buttons--link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Demo
+            </a>
+            <a
+              href="https://www.linkedin.com/in/nayra-romero-salas/"
+              className="container__buttons--link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Repo
+            </a>
+          </div>
+        </div>
       </section>
     </>
   );
